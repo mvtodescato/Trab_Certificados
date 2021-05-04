1d0051ea008faee5532ce9d45f1c48b2e74b9fd3
mvtodescato <matheus.todescato@estudante.uffs.edu.br>
2021-05-04 12:57:03 -0300

    initial commit

---
+++ certificados.py
@@ -0,0 +1,26 @@
+#Trabalho de Certificados - Segurança
+#O sistema deve ser capaz de:
+#
+#A. Assinar documentos digitalmente, a partir de uma chave privada e um certificado associado.
+#
+#B. Gerar certificados de dois tipos:
+#1. Certificados autoassinados.
+#2. Certificados assinados com uma chave associada a um certificado preexistente.
+#
+#C. De posse de um conjunto de certificados, verificar se um documento assinado digitalmente, é válido.
+
+def assinar():
+    print("teste")
+
+
+def gerar_certificados():
+    print("teste")
+    
+def gerar_autoassinados():
+    print("teste")
+    
+def verifica_valido():
+    print("teste")
+    
+if __name__ == '__main__':
+    print('teste')
